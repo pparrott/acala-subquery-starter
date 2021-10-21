9ec29a0597286fbd9c79c2850394bf5b3718a726
pparrott <philparrott8942@gmail.com>
2021-10-21 09:16:33 +0200

    no liquidity bug fix

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -78,7 +78,7 @@ async function updateDailyPool(dailyPool: LiquidityDailySummary, token0Amt: stri
   dailyPool.token0DailyTotal = (BigInt(dailyPool.token0DailyTotal) + addFactor * BigInt(token0Amt)).toString();
   dailyPool.token1DailyTotal = (BigInt(dailyPool.token1DailyTotal) + addFactor * BigInt(token1Amt)).toString();
 
-  dailyPool.save()
+  await dailyPool.save()
 
   return
 }
@@ -160,13 +160,14 @@ async function handleLiquidityEvent(event: SubstrateEvent, add_remove: string): 
   } = event;
   // convert event time to 'YYYYMMDD'
   const eventTime = BigInt(event.extrinsic.block.timestamp.getTime());
-  const eventTimeDate = new Date(Number(eventTime));
-  const eventTimeInt = convertTime(eventTimeDate);
+  // const eventTimeDate = new Date(Number(eventTime));
+  // const eventTimeInt = convertTime(eventTimeDate);
   // parse token values
   const token0Parse = getLiquidityToken(token0);
   const token1Parse = getLiquidityToken(token1);
   // return daily pool level for gieven tokens and update
-  let dailyPool = await getDailyPool(token0Parse, token1Parse, eventTimeInt.toString());
+  // let dailyPool = await getDailyPool(token0Parse, token1Parse, eventTimeInt.toString());
+  let dailyPool = await getDailyPool(token0Parse, token1Parse, eventTime.toString());
   await updateDailyPool(dailyPool, token0Amt.toString(), token1Amt.toString(), add_remove);
 
   return 
